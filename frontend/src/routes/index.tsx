b873039a5ef14f9b4c23216a1eda1b8120b0b659
SK-Subroto <subroto.sks4@gmail.com>
2025-05-02 00:08:16 +0200

    file upload functionality added using dropzone
    Signed-off-by: SK-Subroto <subroto.sks4@gmail.com>

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
+import FileDropzone from '../components/FileDropzone';
+
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+
+  return (
+    <div>
+      <FileDropzone />
+    </div>
+  );
 }
